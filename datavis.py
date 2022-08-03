d840f2057cb3f9532529daddf2ffec0a8ebf5103
farahabdelli <farah.abdelli@esprit.tn>
2022-08-03 15:49:11 +0200

    aa

--- datavis.py
+++ datavis.py
@@ -20,13 +20,17 @@ choix_page = st.sidebar.radio(label="", options=PAGES)
 
 ############# Page 1 #############
 if choix_page == "Accueil":
-    st.markdown('<p class="first_titre">Piscines Magiline </p>', unsafe_allow_html=True)
+    st.write("""
+# Piscines Magiline
+This app predicts the **Iris flower** type!
+""")
+   
     st.write("---")
     c1, c2 = st.columns((3, 2))
     with c2:
         st.write("##")
         st.write("##")
-        st.image("logo/background.png")
+        st.image("image/magiline.png")
     st.write("##")
     with c1:
         st.write("##")
@@ -43,9 +47,8 @@ if choix_page == "Accueil":
     c1, _, c2, _, _, _ = st.columns(6)
     with c1:
         st.subheader("Liens")
-        st.write(
-            "• [Mon profil GitHub](https://github.com/antonin-lfv/Online_preprocessing_for_ML/blob/master/README.md)")
-        st.write("• [Mon site](https://antonin-lfv.github.io)")
+
+        st.write("• [Le site](https://www.piscines-magiline.fr)")
     with c2:
         lottie_accueil = load_lottieurl('https://assets2.lottiefiles.com/packages/lf20_xRmNN8.json')
         st_lottie(lottie_accueil, height=200)
@@ -61,7 +64,7 @@ elif choix_page == "Prédiction":
     st.sidebar.radio(label="", options=PAGES_Prédiction, key="choix_page_classification")
 
     if st.session_state.choix_page_classification == "RF":
-        st.markdown('<p class="grand_titre">RF : Random Forest</p>', unsafe_allow_html=True)
+        st.write("""# Random Forest""")
         st.write("##")
         exp1, exp2, exp3 = st.columns((0.2, 1, 0.2))
         with exp2:
@@ -74,46 +77,44 @@ elif choix_page == "Prédiction":
                 * 5ème étape : Attribution de la classe la plus présente à notre point 
                 """)
 
+        st.sidebar.header('User Input Parameters')
 
-        if 'data' in st.session_state:
-            st.sidebar.header('User Input Parameters')
+        def user_input_features():
+            sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+            sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+            petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+            petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+            data = {'sepal_length': sepal_length,
+                    'sepal_width': sepal_width,
+                    'petal_length': petal_length,
+                    'petal_width': petal_width}
+            features = pd.DataFrame(data, index=[0])
+            return features   
 
-            def user_input_features():
-                sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
-                sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
-                petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
-                petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
-                data = {'sepal_length': sepal_length,
-                        'sepal_width': sepal_width,
-                        'petal_length': petal_length,
-                        'petal_width': petal_width}
-                features = pd.DataFrame(data, index=[0])
-                return features   
+        df = user_input_features()
 
-            df = user_input_features()
+        st.subheader('User Input parameters')
+        st.write(df)
 
-            st.subheader('User Input parameters')
-            st.write(df)
+        iris = datasets.load_iris()
+        X = iris.data
+        Y = iris.target
 
-            iris = datasets.load_iris()
-            X = iris.data
-            Y = iris.target
+        clf = RandomForestClassifier()
+        clf.fit(X, Y)
 
-            clf = RandomForestClassifier()
-            clf.fit(X, Y)
+        prediction = clf.predict(df)
+        prediction_proba = clf.predict_proba(df)
 
-            prediction = clf.predict(df)
-            prediction_proba = clf.predict_proba(df)
+        st.subheader('Class labels and their corresponding index number')
+        st.write(iris.target_names)
 
-            st.subheader('Class labels and their corresponding index number')
-            st.write(iris.target_names)
+        st.subheader('Prediction')
+        st.write(iris.target_names[prediction])
+        #st.write(prediction)
 
-            st.subheader('Prediction')
-            st.write(iris.target_names[prediction])
-            #st.write(prediction)
-
-            st.subheader('Prediction Probability')
-            st.write(prediction_proba)
+        st.subheader('Prediction Probability')
+        st.write(prediction_proba)
 
 
    
