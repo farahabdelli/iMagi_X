8c1da3bc7780a18abf5456a5db641a157d06f7c6
farahabdelli <farah.abdelli@esprit.tn>
2022-08-03 15:53:42 +0200

    test datavis

--- datavis.py
+++ datavis.py
@@ -30,7 +30,7 @@ This app predicts the **Iris flower** type!
     with c2:
         st.write("##")
         st.write("##")
-        st.image("image/magiline.png")
+        st.image("images/magiline.png")
     st.write("##")
     with c1:
         st.write("##")
@@ -116,16 +116,64 @@ elif choix_page == "Prédiction":
         st.subheader('Prediction Probability')
         st.write(prediction_proba)
 
+    elif st.session_state.choix_page_classification == "SVM":
+        st.write("""# Support Vector Machine""")
+        st.write("##")
+        exp1, exp2, exp3 = st.columns((0.2, 1, 0.2))
+        with exp2:
+            with st.expander("Principe de l'algorithme SVM"):
+                st.write("""
+                * 1ère étape : Choix du nombre de voisins k
+                * 2ème étape : Calcul de la distance entre le point non classifié et tous les autre
+                * 3ème étape : Sélection des k plus proches voisins
+                * 4ème étape : On compte le nombre de voisins dans chaque classe
+                * 5ème étape : Attribution de la classe la plus présente à notre point 
+                """)
+
+        st.sidebar.header('User Input Parameters')
+
+        def user_input_features():
+            sepal_length = st.sidebar.slider('Sepal length', 4.3, 7.9, 5.4)
+            sepal_width = st.sidebar.slider('Sepal width', 2.0, 4.4, 3.4)
+            petal_length = st.sidebar.slider('Petal length', 1.0, 6.9, 1.3)
+            petal_width = st.sidebar.slider('Petal width', 0.1, 2.5, 0.2)
+            data = {'sepal_length': sepal_length,
+                    'sepal_width': sepal_width,
+                    'petal_length': petal_length,
+                    'petal_width': petal_width}
+            features = pd.DataFrame(data, index=[0])
+            return features   
+
+        df = user_input_features()
+
+        st.subheader('User Input parameters')
+        st.write(df)
+
+        iris = datasets.load_iris()
+        X = iris.data
+        Y = iris.target
+
+        clf = RandomForestClassifier()
+        clf.fit(X, Y)
+
+        prediction = clf.predict(df)
+        prediction_proba = clf.predict_proba(df)
+
+        st.subheader('Class labels and their corresponding index number')
+        st.write(iris.target_names)
+
+        st.subheader('Prediction')
+        st.write(iris.target_names[prediction])
+        #st.write(prediction)
+
+        st.subheader('Prediction Probability')
+        st.write(prediction_proba)
+
 
    
 ############# Fin Classification #############
 
 
-
-st.write("""
-# Simple Iris Flower Prediction App
-This app predicts the **Iris flower** type!
-""")
 
 
 
